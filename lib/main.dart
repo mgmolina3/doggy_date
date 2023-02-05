84ed81e531cd003787dd7e07c49da1927ce5a8d4
Montserrat Molina <mgmolina3@miners.utep.edu>
2023-02-05 16:59:56 -0700

    added create account functionality and theme to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:doggy_date/profile_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
+import 'screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,8 +12,70 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: const HomePage(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 251, 231, 239),
+        fontFamily: 'ComingSoon',
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: const TextStyle(
+            color: Color.fromARGB(202, 105, 48, 23),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              width: 2,
+              color: Color.fromARGB(180, 211, 156, 129),
+            ),
+            borderRadius: BorderRadius.circular(7.0),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              width: 2,
+              color: Color.fromARGB(203, 84, 38, 18),
+            ),
+            borderRadius: BorderRadius.circular(7.0),
+          ),
+          focusedErrorBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 2,
+              color: Colors.redAccent,
+            ),
+          ),
+          errorBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 2,
+              color: Colors.redAccent,
+            ),
+          ),
+        ),
+        textTheme: const TextTheme(bodyText2: TextStyle()).apply(
+          bodyColor: const Color.fromARGB(203, 84, 38, 18),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            textStyle: MaterialStateProperty.all(
+              const TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontSize: 22.0,
+                fontFamily: 'ComingSoon',
+              ),
+            ),
+            backgroundColor: MaterialStateProperty.all(
+                const Color.fromARGB(202, 224, 44, 125)),
+            elevation: MaterialStateProperty.all(0.0),
+            padding: MaterialStateProperty.all(
+              const EdgeInsets.symmetric(
+                vertical: 20.0,
+              ),
+            ),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -42,146 +103,12 @@ class _HomePageState extends State<HomePage> {
         future: _initializeFirebase(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return LoginScreen();
+            return const LoginScreen();
           }
           return const Center(
             child: CircularProgressIndicator(),
           );
         },
-      ),
-    );
-  }
-}
-
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  // Login function
-  static Future<User?> loginUsingEmailPassword(
-      {required String email,
-      required String password,
-      required BuildContext context}) async {
-    FirebaseAuth auth = FirebaseAuth.instance;
-    User? user;
-    try {
-      UserCredential userCredential = await auth.signInWithEmailAndPassword(
-          email: email, password: password);
-      user = userCredential.user;
-    } on FirebaseAuthException catch (e) {
-      if (e.code == "user-not-found") {
-        print("No user found for that email");
-      }
-    }
-    return user;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    TextEditingController _emailController = TextEditingController();
-    TextEditingController _passwordController = TextEditingController();
-
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'MyApp Title',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const Text(
-            "Login to your app",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 44.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(
-            height: 44.0,
-          ),
-          TextField(
-            controller: _emailController,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              hintText: "User Email",
-              prefixIcon: Icon(Icons.mail, color: Colors.black),
-            ),
-          ),
-          const SizedBox(
-            height: 26.0,
-          ),
-          TextField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: const InputDecoration(
-              hintText: "User Password",
-              prefixIcon: Icon(
-                Icons.security,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 10.0,
-          ),
-          const Text(
-            "Don't remember your password?",
-            style: TextStyle(
-              color: Colors.blue,
-            ),
-          ),
-          const SizedBox(
-            height: 88.0,
-          ),
-          Container(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () async {
-                User? user = await loginUsingEmailPassword(
-                    email: _emailController.text,
-                    password: _passwordController.text,
-                    context: context);
-                print(user);
-                if (user != null) {
-                  Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => ProfileScreen()));
-                }
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Color(0xFF0069FE)),
-                elevation: MaterialStateProperty.all(0.0),
-                padding: MaterialStateProperty.all(
-                  const EdgeInsets.symmetric(
-                    vertical: 20.0,
-                  ),
-                ),
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                  ),
-                ),
-              ),
-              child: const Text(
-                'Login',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
-                ),
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
